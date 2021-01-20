79dac044469130923cda35a62e4c9eaa00498597
Gilvan <ti.gilvan@gmail.com>
2021-01-20 16:55:52 -0300

    Projeto com nova estrutura

---
+++ source/src/app/pages/pages-menu.ts
@@ -0,0 +1,15 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  
+  {
+    title: 'IoT Dashboard',
+    icon: 'home-outline',
+    link: '/',
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+ 
+];
---
+++ source/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,30 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ECommerceComponent } from './e-commerce/e-commerce.component';
+import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [{
+  path: '',
+  component: PagesComponent,
+  children: [    
+    {
+      path: '',
+      component: InicioComponent
+    },
+    {
+      path: '**',
+      component: NotFoundComponent,
+    },
+  ],
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {
+}
